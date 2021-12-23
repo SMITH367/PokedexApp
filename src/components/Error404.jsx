41f549445d957dc0d0fa7376f98b989fc4f03261
Brayan Puentes <brayansmithpuente@gmail.com>
2021-12-23 15:58:39 -0500

    added  error 404

---
+++ src/components/Error404.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Header } from "./UIcomponents/Header";
+
+import { Link } from "react-router-dom";
+const Error404 = () => {
+  return (
+    <>
+      <Header />
+
+      <h1 className="center">
+        Uppps parece que no encontramos a ruta que buscas!
+      </h1>
+      <h2 className="center">
+        <Link to="/">Sacame de aqui!</Link>
+      </h2>
+    </>
+  );
+};
+
+export { Error404 };
